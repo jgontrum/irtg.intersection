5682a8f877f788f5eeecba3082bba073197a1fde
Johannes Gontrum <johannes.gontrum@uni-potsdam.de>
2014-10-13 13:33:18 +0200

    A*

--- src/main/java/de/up/ling/irtg/automata/astar/Estimator.java
+++ src/main/java/de/up/ling/irtg/automata/astar/Estimator.java
@@ -6,8 +6,11 @@
 
 package de.up.ling.irtg.automata.astar;
 
-import java.util.List;
-import java.util.function.Consumer;
+import de.up.ling.irtg.automata.Rule;
+import de.up.ling.irtg.automata.TreeAutomaton;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import javafx.util.Pair;
 
 /**
  *
@@ -17,9 +20,17 @@ import java.util.function.Consumer;
  * @param <InsideSummary>
  */
 public interface Estimator<State, InsideSummary, OutsideSummary> {
-    void forEachRuleOutside(OutsideSummary outsideSummary, int symbol, int arity, int position, Consumer<List<Object>> todo);
-    void forEachRuleInside(InsideSummary insideSummary, Consumer<List<InsideSummary>> todo);    
+    void forEachRuleOutside(OutsideSummary outsideSummary, int symbol, int arity, int position, BiConsumer<OutsideSummary, Pair<InsideSummary, Integer>> todo);
+    void forEachRuleInside(InsideSummary insideSummary, BiConsumer<InsideSummary, InsideSummary>  todo);    
     
     boolean isOutsideSummaryComplete(OutsideSummary outsideSummary);
     boolean isInsideSummaryTerminal(InsideSummary insideSummary);
+    
+    boolean isTerminal(int state);
+    Set<Rule> getRulesForRHSState(int state);
+    TreeAutomaton<State> getGrammar();
+    
+    
+    InsideSummary getSummary(int span);
+
 }
--- src/main/java/de/up/ling/irtg/automata/astar/SXEstimator.java
+++ src/main/java/de/up/ling/irtg/automata/astar/SXEstimator.java
@@ -6,6 +6,7 @@
 
 package de.up.ling.irtg.automata.astar;
 
+import de.up.ling.irtg.algebra.StringAlgebra;
 import de.up.ling.irtg.automata.Rule;
 import de.up.ling.irtg.automata.TreeAutomaton;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
@@ -16,7 +17,9 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import javafx.util.Pair;
 
 /**
  *
@@ -27,19 +30,48 @@ public class SXEstimator<State> implements Estimator<State, SXInside, SXOutside>
     private final TreeAutomaton<State> grammar;
     private Int2ObjectMap<Set<Rule>> rhsSymbolToRules;  //< maps a symbol to a set of rules, where it occurs on the rhs
     private IntSet terminalSymbols;                     //< set of all symbols, that are the parent of a 0-ary rule.
+    private SXSummarizer summarizer;
     
     public SXEstimator(TreeAutomaton<State> grammar) {
         this.grammar = grammar;
         // TODO assert grammar in CNF
         sortRulesByRHS();
         fetchTerminalSymbols();
+        printRhsSymbolToRules();
+        summarizer = null;
     }
     
+    public void setSummarizer(SXSummarizer summarizer) {
+        this.summarizer = summarizer;
+    }
+    
+    @Override
+    public SXInside getSummary(int span) {
+        if (summarizer != null) {
+            return summarizer.summarizeInside(new StringAlgebra.Span(0, span));
+        } else return null;
+    }
+    
+    @Override
+    public TreeAutomaton<State> getGrammar() {
+        return grammar;
+    }
+    
+    @Override
+    public Set<Rule> getRulesForRHSState(int state) {
+        return rhsSymbolToRules.get(state);
+    }
+    
+    @Override
+    public boolean isTerminal(int state) {
+        return terminalSymbols.contains(state);
+    }
+       
     private void sortRulesByRHS() {
         // Calculation of the outside estimate requires access to rules that have 
         // a certain state on their rhs.
         rhsSymbolToRules = new Int2ObjectOpenHashMap<>();
-        rhsSymbolToRules.defaultReturnValue(new HashSet<Rule>());
+        rhsSymbolToRules.defaultReturnValue(new HashSet<>());
         for (Rule r : grammar.getRuleIterable()) {
             for (int symbol : r.getChildren()) {
                 if (!rhsSymbolToRules.containsKey(symbol)) {
@@ -54,10 +86,12 @@ public class SXEstimator<State> implements Estimator<State, SXInside, SXOutside>
     }
 
     private void fetchTerminalSymbols() {
+        // collect all terminal symbols.
+        // The grammar is in CNF, therefore terminals ... TODO
         terminalSymbols = new IntOpenHashSet();
         grammar.getRuleIterable().forEach((Rule r) -> {
             if (r.getArity() == 0) {
-                terminalSymbols.add(r.getParent());
+                terminalSymbols.add(r.getParent()); // not r.rhs[0]?
             }
         });
     }
@@ -72,20 +106,18 @@ public class SXEstimator<State> implements Estimator<State, SXInside, SXOutside>
     
     
     @Override
-    public void forEachRuleOutside(SXOutside outsideSummary, int symbol, int arity, int position, Consumer<List<Object>> todo) {
+    public void forEachRuleOutside(SXOutside outsideSummary, int symbol, int arity, int position, BiConsumer<SXOutside, Pair<SXInside, Integer>> todo) {
         // outsideSummary is left child
         // e.g.: for forEachRule((3,2), *, 2, 0, todo), call “todo” on 
         //      (3,1) -> *((3,2), 1); 
         //      (3,0) -> *((3,2), 2); etc.
-
         for (int i = 1; i <= outsideSummary.getWordsRight(); ++i) {
-            List<Object> ret = new ArrayList<>();
-            ret.add(new SXOutside(outsideSummary.getWordsLeft(), outsideSummary.getWordsRight() - i));
-            ret.add(symbol);
-
-            ret.add(outsideSummary); // do I have to add this to the list? It has to be skipped in estimateOutside anyway?
-            ret.add(new SXInside(i)); // i = length = inside?
-            todo.accept(ret);
+            todo.accept(
+                    // Outside Summary for the parent state, assuming the given outside summary is the LEFT child
+                    new SXOutside(outsideSummary.getWordsLeft(), outsideSummary.getWordsRight() - i),
+                    // Inside Summary for the state at the second position (= 1), because we assume the given outside summary is the LEFT child
+                    new Pair<>(new SXInside(i), 1) 
+            );
         }
 
         // outsideSummary is right child
@@ -93,22 +125,25 @@ public class SXEstimator<State> implements Estimator<State, SXInside, SXOutside>
         //      (2, 2) -> *(1, (3,2)); 
         //      (1, 2) -> *(2, (3,2)); etc.
         for (int i = 1; i <= outsideSummary.getWordsLeft(); ++i) {
-            List<Object> ret = new ArrayList<>();
-            ret.add(new SXOutside(outsideSummary.getWordsLeft() - i, outsideSummary.getWordsRight()));
-            ret.add(symbol);
-            ret.add(new SXInside(i)); // i = length = inside?
-            ret.add(outsideSummary);
-            todo.accept(ret);
+            todo.accept(
+                    // Outside Summary for the parent state, assuming the given outside summary is the RIGHT child
+                    new SXOutside(outsideSummary.getWordsLeft() - i, outsideSummary.getWordsRight()),
+                    // Inside Summary for the state at the first position (= 0), because we assume the given outside summary is the RIGHT child
+                    new Pair<>(new SXInside(i), 0) 
+            );        
         }
     }
 
     @Override
-    public void forEachRuleInside(SXInside insideSummary, Consumer<List<SXInside>> todo) {
-        List<SXInside> ret = new ArrayList<>();
+    public void forEachRuleInside(SXInside insideSummary, BiConsumer<SXInside, SXInside> todo) {
+//        System.err.println("forEachRuleInside: running for " + insideSummary);
         for (int split = 1; split < insideSummary.getSpan(); ++split) {
-            ret.add(new SXInside(split));
-            ret.add(new SXInside(insideSummary.getSpan() - split));
-            todo.accept(ret);
+//            System.err.println("forEachRuleInside: current split = " + split);
+//            System.err.println("forEachRuleInside: creating " + new SXInside(split));
+//            System.err.println("forEachRuleInside: creating " + new SXInside(insideSummary.getSpan() - split));
+
+            todo.accept(new SXInside(split), new SXInside(insideSummary.getSpan() - split));
+
         }
     }
     
@@ -119,7 +154,16 @@ public class SXEstimator<State> implements Estimator<State, SXInside, SXOutside>
 
     @Override
     public boolean isInsideSummaryTerminal(SXInside insideSummary) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return insideSummary.getSpan() == 1;
+    }
+    
+    private void printRhsSymbolToRules() {
+        rhsSymbolToRules.keySet().forEach(key -> {
+            rhsSymbolToRules.get(key).forEach(rule -> {
+                System.out.println(key + " |->  '" + rule.toString(grammar) + "'");
+            });
+        });
+
     }
 
    
