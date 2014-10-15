6e5a0dc88e6d7ce4445aa0321736b7e28df743eb
Johannes Gontrum <johannes.gontrum@uni-potsdam.de>
2014-10-15 17:06:27 +0200

    Renamed 'Estimator' to 'AlgebraStructureSummary'

---
+++ src/main/java/de/up/ling/irtg/automata/astar/AlgebraStructureSummary.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.up.ling.irtg.automata.astar;
+
+import java.util.function.BiConsumer;
+import javafx.util.Pair;
+
+/**
+ *
+ * @author Johannes Gontrum <gontrum@uni-potsdam.de>
+ * @param <OutsideSummary>
+ * @param <InsideSummary>
+ */
+public interface AlgebraStructureSummary<InsideSummary, OutsideSummary> {
+    void forEachRuleOutside(OutsideSummary outsideSummary, int symbol, int arity, int position, BiConsumer<OutsideSummary, Pair<InsideSummary, Integer>> todo);
+    void forEachRuleInside(InsideSummary insideSummary, BiConsumer<InsideSummary, InsideSummary>  todo);    
+    
+    boolean isOutsideSummaryComplete(OutsideSummary outsideSummary);
+    boolean isInsideSummaryTerminal(InsideSummary insideSummary);
+
+}
